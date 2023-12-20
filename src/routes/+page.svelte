3d3751beba7dbd08a4ec82490cf4f7fbdd829ca7
Nicolas-Saavedra <personal@nicolassaavedra.com>
2023-12-20 00:13:40 -0500

    feat: added Experience.svelte, tiled bg scss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import About from '../components/home/About.svelte';
 	import Hero from '../components/home/Hero.svelte';
+	import Experience from '../components/home/Experience.svelte';
 </script>
 
 <Hero />
 <section id="about">
 	<About />
 </section>
-<section id="experience"></section>
+<section id="experience">
+	<Experience />
+</section>
 <section id="contact"></section>
