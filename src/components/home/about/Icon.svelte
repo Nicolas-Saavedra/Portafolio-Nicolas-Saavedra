5f6668d180fff03ae76c48c928f83aedc41584a4
Nicolas-Saavedra <personal@nicolassaavedra.com>
2023-12-20 12:49:52 -0500

    chore: refactor var into inline ternary

--- src/components/home/about/Icon.svelte
+++ src/components/home/about/Icon.svelte
@@ -4,8 +4,6 @@
 	export let index: number;
 
 	let widthClass = 'w-42';
-	let color = index % 2 === 0 ? '700' : '800';
-
 	if (content.length <= 6) {
 		widthClass = 'w-32';
 	} else if (content.length <= 8) {
@@ -16,7 +14,7 @@
 <div
 	class={`h-12 flex flex-row align-middle items-center justify-center px-4
   text-white rounded-2xl mr-4 lg:mr-6 mt-6 shadow-sm
-  ${widthClass} bg-slate-${color}`}
+  ${widthClass} bg-slate-${index % 2 === 0 ? '700' : '800'}`}
 >
 	<img src={iconUrl} style="max-width: 28px;max-height: 28px;" class="mr-3" alt="Language" />
 	<p>
