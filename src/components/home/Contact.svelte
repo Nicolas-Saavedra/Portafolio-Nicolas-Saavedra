85d70ea77e72e6dec7088bfcaa1d465922f58818
Nicolas-Saavedra <personal@nicolassaavedra.com>
2023-12-20 22:56:07 -0500

    feat: fixed up contact form, separated.

--- src/components/home/Contact.svelte
+++ src/components/home/Contact.svelte
@@ -1,37 +1,17 @@
 <script lang="ts">
+	import ContactForm from './contact/ContactForm.svelte';
 	import MediaArray from './contact/MediaArray.svelte';
 </script>
 
 <div
-	class="w-screen py-36 md:px-28 lg:px-64 bg-soft-white flex flex-col align-middle justify-center items-center"
+	class="w-screen pt-36 pb-16 md:px-28 lg:px-64 bg-soft-white flex flex-col align-middle justify-center items-center"
 >
 	<div id="title-container flex flex-col">
 		<h1 class="text-6xl font-lato tracking-wider">Contactame</h1>
 		<span class="block h-[5px] w-[2.25rem] bg-slate-800"></span>
 	</div>
-	<div class="w-3/5 flex justify-center mt-20 bg-white rounded-3xl">
-		<form class="mt-16 mb-24 w-5/6" action="#" method="POST">
-			<div class="flex flex-col">
-				<div class="mt-4">
-					<label class="block text-lg font-source font-bold" for="name">Nombre</label>
-					<input class="block w-full h-12 mt-1 rounded-md bg-gray-200" id="name" type="text" />
-				</div>
-				<div class="mt-4">
-					<label class="block text-lg font-source font-bold" for="email">Correo Electronico</label>
-					<input class="block w-full h-12 mt-1 rounded-md bg-gray-200" id="email" type="text" />
-				</div>
-				<div class="mt-4">
-					<label class="block text-lg font-source font-bold" for="reason">Motivo</label>
-					<input class="block w-full h-12 mt-1 rounded-md bg-gray-200" id="reason" type="text" />
-				</div>
-				<div class="mt-4">
-					<label class="block text-lg font-source font-bold" for="content"
-						>Informacion Adicional</label
-					>
-					<input class="block w-full h-12 mt-1 rounded-md bg-gray-200" id="content" type="text" />
-				</div>
-			</div>
-		</form>
+	<div class="w-3/5 flex justify-center mt-16 bg-white rounded-3xl">
+		<ContactForm />
 	</div>
 	<MediaArray />
 </div>
---
+++ src/components/home/contact/ContactForm.svelte
@@ -0,0 +1,44 @@
+<form class="my-8 w-5/6" action="#" method="POST">
+	<div class="flex flex-col">
+		<div class="mt-4">
+			<label class="block text-lg font-source font-bold" for="name">Nombre</label>
+			<input
+				class="block w-full h-12 py-2 px-4 mt-1 rounded-md bg-gray-200 outline-none"
+				id="name"
+				type="text"
+				placeholder="Ingrese su nombre"
+			/>
+		</div>
+		<div class="mt-4">
+			<label class="block text-lg font-source font-bold" for="email">Correo Electronico</label>
+			<input
+				class="block w-full h-12 py-2 px-4 mt-1 rounded-md bg-gray-200 outline-none"
+				id="email"
+				type="text"
+				placeholder="Ingrese su correo electronico"
+			/>
+		</div>
+		<div class="mt-4">
+			<label class="block text-lg font-source font-bold" for="reason">Motivo</label>
+			<input
+				class="block w-full h-12 py-2 px-4 mt-1 rounded-md bg-gray-200 outline-none"
+				id="reason"
+				type="text"
+				placeholder="Ingrese el motivo de su solicitud"
+			/>
+		</div>
+		<div class="mt-4">
+			<label class="block text-lg font-source font-bold" for="content">Informacion Adicional</label>
+			<textarea
+				class="block w-full h-[250px] py-4 px-4 mt-1 rounded-md bg-gray-200 border-none resize-none outline-none"
+				id="content"
+				placeholder="Ingrese informacion adicional pertinente..."
+			/>
+		</div>
+		<div class="mt-8 flex justify-center">
+			<button class="font-source font-bold text-lg bg-slate-700 text-white py-4 px-6 rounded-3xl"
+				>Enviar Solicitud</button
+			>
+		</div>
+	</div>
+</form>
