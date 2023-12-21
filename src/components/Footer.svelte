4a05023f037f502dc63cdb3af1dd56a18806899b
Nicolas-Saavedra <personal@nicolassaavedra.com>
2023-12-20 22:37:46 -0500

    feat: finished footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="w-screen h-72 bg-slate-800 py-16 px-64 text-white">
+	<p class="font-lato text-xl">Nicolas Saavedra Gonzalez</p>
+	<p class="font-lato text-md mt-4">Copyright 2023 ©</p>
+	<hr class="mt-8 h-px border-none bg-slate-600" />
+	<p class="text-sm text-slate-500 mt-8 text-center">
+		Made by hand in <a class="underline" href="https://kit.svelte.dev/">SvelteKit</a>
+	</p>
+</footer>
